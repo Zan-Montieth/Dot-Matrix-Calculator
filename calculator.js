b7f2dfc114fda1c63308cb304487e3bc248e62fe
Zan <zan.montieth@gmail.com>
2023-04-10 04:55:46 -0600

    add activation and deactivation functions for dots in the matrix

--- calculator.js
+++ calculator.js
@@ -1,6 +1,7 @@
 const displayXdimension = 112
 const displayYdimension = 24
 
+
 function createDotMatrix(){
     let dotDisplay = document.getElementById("dotDisplay")
     let dotElement = document.createElement("div");
@@ -8,16 +9,20 @@ function createDotMatrix(){
     let rowElement = document.createElement("div");
     rowElement.classList.add("row")
     for(dot = 0; dot < displayXdimension; dot++){
+        dotElement.classList.remove(dot - 1)
+        dotElement.classList.add(dot)
         rowElement.appendChild(dotElement.cloneNode(true));
     }
     for(row = 0; row < displayYdimension; row++){
+        rowElement.classList.remove(row - 1)
+        rowElement.classList.add(row)
         dotDisplay.appendChild(rowElement.cloneNode(true));
     }
     sizeDotDisplay();
 }
 
 function sizeDotDisplay() {
-    const sizeContainer = document.getElementById("dotDisplay");
+    const dotDisplay = document.getElementById("dotDisplay");
     const dotElement = document.getElementsByClassName("dot")[0];
     let dotWidth = getComputedStyle(dotElement).getPropertyValue("width");
     dotWidth = dotWidth.substring(0, dotWidth.length - 2);
@@ -27,5 +32,15 @@ function sizeDotDisplay() {
     let newWidth = dotWidth * displayXdimension + dotMargin * 2 * displayXdimension;
     newWidth = newWidth + "px";
     console.log(newWidth)
-    sizeContainer.style.width = newWidth;
+    dotDisplay.style.width = newWidth;
+}
+
+function activateDot(xCoord, yCoord){
+    const dotDisplay = document.getElementById("dotDisplay");
+    dotDisplay.childNodes[yCoord].childNodes[xCoord].classList.add("activated")
+}
+
+function deactivateDot(xCoord, yCoord){
+    const dotDisplay = document.getElementById("dotDisplay");
+    dotDisplay.childNodes[yCoord].childNodes[xCoord].classList.remove("activated")
 }
